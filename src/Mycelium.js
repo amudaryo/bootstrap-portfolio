bfca74fba28f118318e3fd63371ce33952b03a74
amudaryo <daria.muracheva@gmail.com>
2021-05-05 20:23:29 +0200

    add github and linkedin button

--- src/Mycelium.js
+++ src/Mycelium.js
@@ -1,9 +1,17 @@
 import Template from './components/Template';
+import Button from 'react-bootstrap/Button'
 
 export default function Mycelium() {
   const title = `title for Mycelium`;
   const text = `... text for Mycelium`;
   const img = './wood-wide-web.jpg';
 
-  return <Template title={title} text={text} image={img} />;
+  const Other = (
+    <div className="network">
+      <Button href="https://www.github.com/amudaryo/"> My GitHub</Button>
+      <Button href="https://www.linkedin.com/in/daria-muracheva-49b3a11b3">My Linkedin</Button>
+    </div>
+  )
+
+  return <Template title={title} text={text} image={img} other={Other}/>;
 }
