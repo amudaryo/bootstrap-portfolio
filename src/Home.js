e93934da80217dddde47981382dd02b9f7d2c6bc
amudaryo <daria.muracheva@gmail.com>
2021-05-06 12:09:35 +0200

    minor changes to spelling

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import Template from './components/Template';
 
 export default function Home() {
   const title = `Hi, I'm Daria`;
-  const text = `... a develloper from berlin`;
+  const text = `... a developer from berlin`;
   const img = './wood-wide-web.jpg';
 
   return <Template title={title} text={text} image={img} />;
